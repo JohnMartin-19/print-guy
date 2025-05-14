7d4ea574daa998ce5fba7af243eaecbe5ddf5c35
John Mburu <johnnkonge2020@gmail.com>
2025-05-14 04:51:36 +0300

    staging for mobile is ready. TEST

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Footer from "./components/Footer";
 import Home from "./pages/Home";
 
 import Contact from "./pages/Contact";
